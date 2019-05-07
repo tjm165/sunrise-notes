2f8cb94a35cb3339abd39e049fcc3cf03646bbd7
Thomas <tjm165@case.edu>
2019-05-07 00:05:13 -0400

    Semantics

--- src/components/desk/DeskItems.jsx
+++ src/components/desk/DeskItems.jsx
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
 import Note from "./Note";
 import { Card } from "../../../node_modules/semantic-ui-react";
+import { Container } from "semantic-ui-react";
 
 class DeskItems extends Component {
   render() {
     const { state, functions } = this.props;
     const tagsInCurrentContext = state.contexts[state.currentContext].tags;
-    const gotti = Array.from(functions.getContextNotes());
+    const notes = Array.from(functions.getContextNotes());
     const notesToRender = [2, 1, 3];
-
-    console.log(gotti);
 
     var i = 0;
     return (
-      <div>
-        Desk Items:
+      <Container>
         <Card.Group>
-          {gotti.map(note => (
+          {notes.map(note => (
             <Note index_o={note} state={state} functions={functions} />
           ))}
         </Card.Group>
-      </div>
+      </Container>
     );
   }
 }
