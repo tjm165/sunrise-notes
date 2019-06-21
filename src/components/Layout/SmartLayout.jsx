3d2267052c59b64d54988ffb2ef2de04be60cf5d
Tommy Moawad <tommymoawad@Tommys-MBP.attlocal.net>
2019-06-21 00:12:58 -0400

    small refactor

--- src/components/Layout/SmartLayout.jsx
+++ src/components/Layout/SmartLayout.jsx
@@ -72,18 +72,11 @@ class SmartLayout extends Component {
   }
 
   async submitActiveNote(activeNote) {
-    console.log(activeNote);
-
-    //const noteMap = this.state.noteMap;
-    //const activeNoteUUID = this.state.activeNoteUUID;
-    //noteMap.set(activeNoteUUID, activeNote);
-    //this.setState({ noteMap: noteMap });
-
     const ask =
       "https://e2y5q3r1l1.execute-api.us-east-2.amazonaws.com/production/note";
 
     this.postData(ask, activeNote)
-      .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+      .then(data => console.log(JSON.stringify(data)))
       .catch(error => console.error(error));
   }
 
