71085bb9ee855842bc778bcb4334291a41528c2f
Tommy Moawad <tommymoawad@Tommys-MBP.attlocal.net>
2019-06-30 18:53:18 -0400

    removed hex method

--- src/objects/Tag.jsx
+++ src/objects/Tag.jsx
@@ -9,25 +9,10 @@ export default class Tag extends Set {
     this.title = title;
     this.rgb = rgb;
     this.UUID = UUID;
-    this.hex = Tag.rgbToHex(this.rgb.r, this.rgb.g, this.rgb.b);
   }
 
   //hmm these should know their uuid too
   static deserialize(json) {
     return new Tag(json["title"], json["noteUUIDs"], json["rgb"], json["UUID"]);
-  }
-
-  static componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  }
-
-  static rgbToHex(r, g, b) {
-    return (
-      "#" +
-      Tag.componentToHex(r) +
-      Tag.componentToHex(g) +
-      Tag.componentToHex(b)
-    );
   }
 }
