6f30efa73d80c02ceee7166c31eed1b39e189cc5
Tommy Moawad <tommymoawad@Tommys-MBP.attlocal.net>
2019-07-07 14:19:38 -0400

    made a link

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Signin extends Component {
   render() {
-    return <div>SIGN IN</div>;
+    return (
+      <div>
+        SIGN IN
+        <Link to="/dashboard">
+          <button>click</button>
+        </Link>
+      </div>
+    );
   }
 }
 
