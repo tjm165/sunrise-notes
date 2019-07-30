55f6d9f9cb9827af89d7ae3426dc7fff22531629
Tommy Moawad <tommymoawad@Tommys-MBP.attlocal.net>
2019-07-30 00:28:43 -0400

    added logo to nav bar

--- src/components/implementations/Layout/NavBar.jsx
+++ src/components/implementations/Layout/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-import { Button, Container, Menu } from "semantic-ui-react";
+import { Button, Container, Menu, Image } from "semantic-ui-react";
 
 function NavBar({ fixed, activeItem, history }) {
   return (
@@ -11,8 +11,12 @@ function NavBar({ fixed, activeItem, history }) {
       secondary={!fixed}
       className="navBar"
       size="large"
+      fluid
     >
       <Container>
+        <Menu.Item as="a" onClick={() => history.push("/")}>
+          <Image src="/logo.png" size="mini" />
+        </Menu.Item>
         <Menu.Item
           as="a"
           active={activeItem === "Home"}
