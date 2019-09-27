a9888cb3a6ce4ecacb8269d7224bb58561677b67
Thomas <tjm165@case.edu>
2019-09-27 14:14:37 -0400

    Operation wording

--- src/components/pages/dashboard/OperationSelector.jsx
+++ src/components/pages/dashboard/OperationSelector.jsx
@@ -4,11 +4,11 @@ import { Form, Radio } from "semantic-ui-react";
 function OperationSelector({ operation, functions }) {
   return (
     <Form>
-      <Form.Field>Operation:</Form.Field>
+      <Form.Field>Show notes with</Form.Field>
       <Form.Field>
         <Radio
           toggle
-          label="Union"
+          label="any"
           name="radioGroup"
           checked={operation === "union"}
           onChange={() => functions.setOperation("union")}
@@ -17,21 +17,13 @@ function OperationSelector({ operation, functions }) {
       <Form.Field>
         <Radio
           toggle
-          label="Intersection"
+          label="all"
           name="radioGroup"
           checked={operation === "intersection"}
           onChange={() => functions.setOperation("intersection")}
         />
       </Form.Field>
-      <Form.Field>
-        <Radio
-          toggle
-          label="Complement"
-          name="radioGroup"
-          checked={operation === "complement"}
-          onChange={() => functions.setOperation("complement")}
-        />
-      </Form.Field>
+      <Form.Field>selected tags</Form.Field>
     </Form>
   );
 }
