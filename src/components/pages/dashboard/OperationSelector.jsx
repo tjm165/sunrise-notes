e514b253cce39bc195fbdd5453bd80eeec23abc6
Thomas <tjm165@case.edu>
2019-09-27 14:45:48 -0400

    started converting op selector to dropdown

--- src/components/pages/dashboard/OperationSelector.jsx
+++ src/components/pages/dashboard/OperationSelector.jsx
@@ -1,30 +1,51 @@
 import React from "react";
-import { Form, Radio } from "semantic-ui-react";
+import { Dropdown, Form, Radio } from "semantic-ui-react";
 
 function OperationSelector({ operation, functions }) {
+  const options = [
+    {
+      key: "union",
+      text: "Any",
+      value: "union",
+      image: { avatar: true, src: "/images/avatar/small/jenny.jpg" }
+    },
+    {
+      key: "intersection",
+      text: "all",
+      value: "intersection",
+      image: { avatar: true, src: "/images/avatar/small/elliot.jpg" }
+    }
+  ];
   return (
-    <Form>
-      <Form.Field>Show notes with</Form.Field>
-      <Form.Field>
-        <Radio
-          toggle
-          label="any"
-          name="radioGroup"
-          checked={operation === "union"}
-          onChange={() => functions.setOperation("union")}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Radio
-          toggle
-          label="all"
-          name="radioGroup"
-          checked={operation === "intersection"}
-          onChange={() => functions.setOperation("intersection")}
-        />
-      </Form.Field>
-      <Form.Field>selected tags</Form.Field>
-    </Form>
+    <>
+      <span>
+        Show me posts by{" "}
+        <Dropdown inline options={options} defaultValue={options[0].value} />
+      </span>
+
+      <Form>
+        <Form.Field>Show notes with</Form.Field>
+        <Form.Field>
+          <Radio
+            toggle
+            label="any"
+            name="radioGroup"
+            checked={operation === "union"}
+            onChange={() => functions.setOperation("union")}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            toggle
+            label="all"
+            name="radioGroup"
+            checked={operation === "intersection"}
+            onChange={() => functions.setOperation("intersection")}
+          />
+        </Form.Field>
+        <Form.Field>selected tags</Form.Field>
+      </Form>
+    </>
   );
 }
 
