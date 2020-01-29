2eaeb20db7d34098d26481b4aa265822482b6ca1
Tommy Moawad <tommymoawad@Tommys-MacBook-Pro.local>
2020-01-28 22:44:56 -0500

    created a privacy policy

---
+++ src/components/pages/PrivacyPolicy.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      This web application is a side project. I am learning to encrypt your data
+      and I will update the page when it has been accomplished. As of January
+      28, 2020.
+    </div>
+  );
+};
+
+export default withRouter(PrivacyPolicy);
