6b3a70426231600b04ae09bf2b1a645c99891ae1
Tommy Moawad <tommymoawad@Tommys-MacBook-Pro.local>
2020-01-29 20:56:00 -0500

    Redirect on sign in

--- src/components/pages/GooglePostSignIn.jsx
+++ src/components/pages/GooglePostSignIn.jsx
@@ -1,37 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import Amplify, { Auth, Hub } from "aws-amplify";
 
-const Google = props => {
-  Hub.listen("auth", data => {
-    switch (data.payload.event) {
-      case "signIn":
-        this.setState({ authState: "signedIn", authData: data.payload.data });
-        break;
-      case "signIn_failure":
-        this.setState({
-          authState: "signIn",
-          authData: null,
-          authError: data.payload.data
-        });
-        break;
-      default:
-        break;
-    }
+function Google(props) {
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().then(user => {
+      document.cookie = `idToken=${user.signInUserSession.idToken.jwtToken}`;
+      props.history.push("/dashboard");
+    });
   });
 
-  return (
-    <button
-      onClick={() =>
-        Auth.currentAuthenticatedUser().then(user => {
-          document.cookie = `idToken=${user.signInUserSession.idToken.jwtToken}`;
-          props.history.push("/dashboard");
-        })
-      }
-    >
-      Google
-    </button>
-  );
-};
+  return <div>Please wait while authenticating with Google</div>;
+}
 
 export default withRouter(Google);
