5e48e1635a220625a2b2af63d18580655ace812b
Tommy Moawad <tommymoawad@Tommys-MBP.attlocal.net>
2020-03-26 13:44:59 -0400

    cosmetic fixes

--- src/components/pages/AboutTeam.jsx
+++ src/components/pages/AboutTeam.jsx
@@ -40,9 +40,8 @@ function AboutTeam({ history }) {
     {
       name: "Tommy M",
       description:
-        "Passionate about designing software while keeping users in mind. Known for being a positive and organized co-op and RA.",
-      imgSrc:
-        "https://lh3.googleusercontent.com/3y4ctAd47C_2ZjSTIxK7AREHztJdPao5BO0zvCmtyy9msXhqrgzXU1TQHTpkMC0OzP9vNLF-QdbVt495aDit3AXg-aw4aeRMaDozFyCZ-O1L4XkqmT6LOV4b_OtGbN7xbN3Wo9LaUTVODYTvgB-DcUvVfREQzY7kFqeLE8C-Su79mmkFrmrI6Zj3bfCDo8e8lf8glowIDsjB-1N6FFnN9_lX5zRMk-fP3yezFF5chpoIAhof7ezQDScIS7sIoHkRtMtyGHne0caRnTD2-NQKAO-DzDpUS_s6rMepWYr7ANYPSDU0av8sCr07HIiv3vSqGTz88Z3B2pFdId9FFG_j4pWPagGGoyQnwK7uAQzgnmoTilSYDKVPTkO7ccguZ2JsHSV-xcbnHKXzG6D7XJU2ti3z4GdLeYAT5PpIbgbaA1IOSiv07iaPcmPygp9EaApXY-60SZV6LbpeQ6_RVC_duUNL11x8aP9MgckoJLC9MQubTL98ng3NlljhEHYoL75EMhv3VzJqdvyVV5v8kmRHTYBINYxpFk4X9UmVrdDNduMUrvdyZ3fxcpru4sa_Tov7oODrNorv5OkW7knD3iDUIyZVjWtd4vGy_EAb8Mia-UOPErydb2xI0gnTLZbHWw36kCPiTUW5y1j9c7pz0ysnVzOS1xHNGwz5emiR_mnBV2WsxGZRQRnrpQ=w2550-h1780-no",
+        "Enjoys working with React, AWS and artificial intelligence. Known for being a creative and organized co-op and RA.",
+      imgSrc: "https://i.imgur.com/2ToLUSx.jpg",
       title: "Owner/Creator",
       contacts: [
         {
